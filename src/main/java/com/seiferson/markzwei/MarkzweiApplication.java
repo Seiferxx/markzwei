5b99f2bd52ddb1c37e05419710b5713bd07b87d3
seiferxx <seifer.ch@gmail.com>
2020-06-26 23:12:56 -0500

    Base spring initializr files

---
+++ src/main/java/com/seiferson/markzwei/MarkzweiApplication.java
@@ -0,0 +1,13 @@
+package com.seiferson.markzwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkzweiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkzweiApplication.class, args);
+	}
+
+}
