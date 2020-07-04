5b13f64a2c2c3d4178c3618e57b5371f6632dff6
seiferxx <seifer.ch@gmail.com>
2020-07-04 00:30:54 -0500

    updated reference

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Markzwei is a markup language based on a **sub-set** of the rules of the popular
 markup language **Markdown** created **by John Gruber**. The main idea behind this project
 is to make the language strict and less ambiguous so it's easier for computers to parse it.
 
-The name comes from the word in English "mark" referring to markup language and
-to Markdown, the language in which this is based, and zwei which means two in German
+The name comes from the word in English "mark" referring to markup language, and "zwei" which means two in German.
+Also the name makes a reference to Markdown, the language in which is based.
 
 ## Components
 
@@ -26,31 +26,65 @@ to Markdown, the language in which this is based, and zwei which means two in Ge
 
 ## Headers
 
-Markzwei only allows 3 levels of headers, instead of the 6 defined on the Markdown rules.
-To mark a line as a header start the line with a **#** followed by an empty space character. 
-Use two and three hash signs respectively for level 2 and 3 headers. Headers can only contain
-text in normal, italics, or bold form. If more than 3 hash signs are used, the line will not 
+Markzwei defines 6 levels of headers, the same ones as defined on the Markdown rules.
+To mark a line as a header start the line with a **#** *(hash sign)* followed by an empty space character. 
+Use up to 6 hash signs respectively for the different levels. Headers can only contain
+text in normal, italics, or bold form. If more than 6 hash signs are used, the line will not 
 be identified as a header. No blank line is required between the preceding and following lines.
 
 ```
 # This is a header level 1
-Will produce : <h1>This is a header level 1</h1>
+Will produce : <header1>This is a header level 1</header1>
 
 ## This is a header level 2
-Will produce : <h2>This is a header level 2</h2>
+Will produce : <header2>This is a header level 2</header2>
 
 ### This is a header level 3
-Will produce : <h3>This is a header level 3</h3>
+Will produce : <header3>This is a header level 3</header3>
 
-#### This is not a header
-Will produce : <p>#### This is not a header</p>
+#### This is a header level 4
+Will produce : <header4>This is a header level 4</header4>
+
+##### This is a header level 5
+Will produce : <header5>This is a header level 5</header5>
+
+###### This is a header level 6
+Will produce : <header6>This is a header level 6</header6>
+
+####### This is not a header
+Will produce : <paragraph>####### This is not a header</paragraph>
+
+#This is not a header
+Will produce : <paragraph><tag>#This</tag> is not a header</paragraph>
+(This case is not recognized as a header because of the lack of space separating the hash from the text)
 ```
 
 **Identification regex**: `^#\ .*$` `^##\ .*$` `^###\ .*$`
 
+**Valid inline elements:** `Italics` `Bold`
+
 ## Horizontal Rule
 
+The horizontal rule is used as a separator. This element works the same as defined on the original reference for
+Markdown but with the difference that it only accepts **\*** *(asterisk)*. The horizontal rule is defined as a line only 
+containing asterisk and blank space characters on the following form: One or more space characters, followed by at least
+three asterisks optionally separated by a space character, succeeded by any amount of space characters. No blank line is 
+required between the preceding and following lines. Even when is valid to use alternated space separated and not separated
+asterisks we suggest to use a consistent format per line to make not parsed text more readable.
 
+```
+ ***
+Will produce: <horizontalrule />
+
+    * * * *
+Will produce: <horizontalrule />
+
+ *  * *** * **
+Will produce: <paragraph> *  * *** * **</paragraph>
+(This case is not recognized as a horizontal rule because it contains a distance of two blank spaces between asterisk characters)
+```
+
+**Identification regex**: `^\s*(?:\*\ ?){2,}\*\s*$`
 
 ## Block quote
 
--- src/main/java/com/seiferson/markzwei/MarkzweiProcessor.java
+++ src/main/java/com/seiferson/markzwei/MarkzweiProcessor.java
@@ -9,27 +9,27 @@ public class MarkzweiProcessor {
         ArrayList<ComponentRule> rules = new ArrayList<>();
 
         ComponentRule textRule = new ComponentRule("text", null, null, null, 0);
-        ComponentRule listItemRule = new ComponentRule("listItem", null, "li", textRule, 0);
+        ComponentRule listItemRule = new ComponentRule("listItem", null, "item", textRule, 0);
 
-        rules.add(new ComponentRule("segment", "^\\s*```\\s*$", "pre", textRule, 0));
-        rules.add(new ComponentRule("header1", "^#\\ .*$", "h1", textRule, 2));
-        rules.add(new ComponentRule("header2", "^##\\ .*$", "h2", textRule, 3));
-        rules.add(new ComponentRule("header3", "^###\\ .*$", "h3", textRule, 4));
-        rules.add(new ComponentRule("horizontalRule", "^\\s*(?:\\*\\ ){2,}\\*\\s*$", "hr", null,  0));
+        rules.add(new ComponentRule("segment", "^\\s*```\\s*$", "segment", textRule, 0));
+        rules.add(new ComponentRule("header1", "^#\\ .*$", "header1", textRule, 2));
+        rules.add(new ComponentRule("header2", "^##\\ .*$", "header2", textRule, 3));
+        rules.add(new ComponentRule("header3", "^###\\ .*$", "header3", textRule, 4));
+        rules.add(new ComponentRule("horizontalRule", "^\\s*(?:\\*\\ ){2,}\\*\\s*$", "horizontalrule", null,  0));
         rules.add(new ComponentRule("blankLine", "^\\s*$", null, null,  0));
         rules.add(new ComponentRule("blockQuote", "^> .*$", "blockquote", textRule, 2));
-        rules.add(new ComponentRule("unorderedList", "^\\ \\ (?:\\+|\\*|\\-)\\ .*$", "ul", listItemRule, 4));
-        rules.add(new ComponentRule("orderedList", "^\\ \\ \\d*\\.\\ .*$", "ol", listItemRule, 5));
-        rules.add(new ComponentRule("spacedText", "\\ {4,}.*", "p", textRule, 0));
-        rules.add(new ComponentRule("paragraph", ".*", "p", textRule, 0));
+        rules.add(new ComponentRule("unorderedList", "^\\ \\ (?:\\+|\\*|\\-)\\ .*$", "unorderedlist", listItemRule, 4));
+        rules.add(new ComponentRule("orderedList", "^\\ \\ \\d*\\.\\ .*$", "orderedlist", listItemRule, 5));
+        rules.add(new ComponentRule("spacedText", "\\ {4,}.*", "paragraph", textRule, 0));
+        rules.add(new ComponentRule("paragraph", ".*", "paragraph", textRule, 0));
 
         return rules;
     }
 
     public static void checkForBoldAndItalics(Component component){
-        component.setText(component.getText().replaceAll("\\*{3}(.*?)\\*{3}", "<b><em>$1</em></b>"));
-        component.setText(component.getText().replaceAll("\\*{2}(.*?)\\*{2}", "<b>$1</b>"));
-        component.setText(component.getText().replaceAll("\\*(.*?)\\*", "<em>$1</em>"));
+        component.setText(component.getText().replaceAll("\\*{3}(.*?)\\*{3}", "<bold/><italics>$1</italics></b>"));
+        component.setText(component.getText().replaceAll("\\*{2}(.*?)\\*{2}", "<bold>$1</bold>"));
+        component.setText(component.getText().replaceAll("\\*(.*?)\\*", "<italics>$1</italics>"));
     }
 
     public static void processInlineComponents(Component component) {
@@ -40,11 +40,11 @@ public class MarkzweiProcessor {
             component.getRule().getComponent().equals("header2") ||
             component.getRule().getComponent().equals("header3"))
         ) {
-            component.setText(component.getText().replaceAll("(^|\\s)(\\[)([^\\]]+?)(\\])(\\()([^\\)]+?)(\\))(?=(\\s|$))", "$1<a target=\"_blank\" href=\"$6\">$3</a>"));
-            component.setText(component.getText().replaceAll("(^|\\s)(!\\[)([^\\]]+?)(\\]\\()([^\\)]+?)(\\))(?=\\s|$)", "$1<figure><img class=\"u-max-full-width\" src=\"$5\" alt=\"$3\" /><figcaption style=\"text-align:center;\"><em>$3</em></figcaption></figure>"));
-            component.setText(component.getText().replaceAll("(^|\\s)(@[^\\s]+)(?=(\\s|$))", "$1<a href=\"#$2\">$2</a>"));
-            component.setText(component.getText().replaceAll("(^|\\s)(#[^\\s]+)(?=(\\s|$))", "$1<a href=\"#$2\">$2</a>"));
-            component.setText(component.getText().replaceAll("`(.*?)`", "<q>$1</q>"));
+            component.setText(component.getText().replaceAll("(^|\\s)(\\[)([^\\]]+?)(\\])(\\()([^\\)]+?)(\\))(?=(\\s|$))", "$1<link url=\"$6\">$3</a>"));
+            component.setText(component.getText().replaceAll("(^|\\s)(!\\[)([^\\]]+?)(\\]\\()([^\\)]+?)(\\))(?=\\s|$)", "$1<image url=\"$5\" />$3<image>"));
+            component.setText(component.getText().replaceAll("(^|\\s)(@[^\\s]+)(?=(\\s|$))", "$1<mention>$2</tag>"));
+            component.setText(component.getText().replaceAll("(^|\\s)(#[^\\s]+)(?=(\\s|$))", "$1<tag>$2</tag>"));
+            component.setText(component.getText().replaceAll("`(.*?)`", "<highlight>$1</highlight>"));
 
         }
     }
@@ -62,7 +62,7 @@ public class MarkzweiProcessor {
                 }
             }
         }
-        ComponentRule rootRule = new ComponentRule("root", null, "div", null, 0);
+        ComponentRule rootRule = new ComponentRule("root", null, "document", null, 0);
 
         Component root = new Component(rootRule, null);
         root.setChildren(components);
@@ -153,26 +153,26 @@ public class MarkzweiProcessor {
             }
         }
 
-        return getText(root).replaceAll("\n</pre", "</code></pre").replaceAll("<pre> \n", "<pre><code>");
+        return getText(root);
 
     }
 
     public static String getText(Component component) {
-        String htmlText = "";
+        String text = "";
 
         if(component.getRule().getComponent().equals("text")){
-            htmlText += component.getText() + "\n";
+            text += component.getText() + "\n";
         } else {
             if(component.getRule().getComponent().equals("horizontalRule")) {
-                htmlText += "<" + component.getRule().getTag() + "/> \n";
+                text += "<" + component.getRule().getTag() + "/> \n";
             } else {
-                htmlText += "<" + component.getRule().getTag() + "> \n";
+                text += "<" + component.getRule().getTag() + "> \n";
                 for (Component child : component.getChildren()) {
-                    htmlText += getText(child);
+                    text += getText(child);
                 }
-                htmlText += "</" + component.getRule().getTag() + "> \n";
+                text += "</" + component.getRule().getTag() + "> \n";
             }
         }
-        return htmlText;
+        return text;
     }
 }
